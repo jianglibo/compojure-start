b060a458a2accb9c3a109ce736050f438700355d
jianglibo <jianglibo@gmail.com>
2014-11-26 13:19:20 +0800

    add some comment

--- src/compojure_start/core/handler.clj
+++ src/compojure_start/core/handler.clj
@@ -10,13 +10,9 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 
-(app-settings/init)
-(db-util/init)
-(sec-util/init)
-
 (defroutes app-routes
-;  (GET "/" [] "Hello World")
-  (GET "/" [] (io/resource "public/index.html"))
+  (GET "/" [] "Hello World")
+;  (GET "/" [] (io/resource "public/index.html"))
   (route/not-found "Not Found"))
 
 (def app
@@ -27,3 +23,10 @@
 ;wrap-defaults total has 18 wraps,
 ;site-defaults is an hash config, which default include: params,cookies,session,security,static,responses
 ;defroutes return an handler.
+
+
+;compujure app start.
+;lein ring server-headless actualy find handler function in project.clj :ring {:handler compojure-start.core.handler/app}
+
+;ring.adapter.jetty expose
+;^Server run-jetty [handler options]
