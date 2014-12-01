688887fa46dd8d4cc0001527bfc209537ed3779d
jianglibo <jianglibo@gmail.com>
2014-12-01 11:47:30 +0800

    refact pressure

--- src/compojure_start/dutil/pressure_util.clj
+++ src/compojure_start/dutil/pressure_util.clj
@@ -1,55 +1,39 @@
 (ns compojure-start.dutil.pressure-util
   (:require [clojure.tools.logging :as log]
+            [clojure.string :as cstr]
             [clj-http.client :as client])
   (:import (java.util.concurrent Executors)))
 
-(def cookie-store (clj-http.cookies/cookie-store))
 
-(def cm (clj-http.conn-mgr/make-reusable-conn-manager {:timeout 2 :threads 3}))
+(defn random-str [size]
+  (cstr/join (take size (repeatedly #(rand-nth "0123456789abcdefghijklmnopqrstuvwxyz")))))
 
-(def home-page "http://oa.fh.gov.cn")
-
-(def no-end-url "http://oa.fh.gov.cn/jwoa4share/jwoa4system/db_printview.nsf/PeoplePrintView?OpenAgent&infoid=mqhb_Info&path=jwoa4share/jwoa4app&dbname=db_mqhb.nsf")
-(def verify-url "http://oa.fh.gov.cn/domcfg.nsf/LoginVerification?OpenAgent&Ran=0.7505126900505275&user=weisj&pd=680012")
-(def index-url "http://oa.fh.gov.cn/index.nsf/Index?OpenForm")
-(def login-url "http://oa.fh.gov.cn/names.nsf?Login")
-
-(defn parse-response
-  [avalue newres url]
-  (let [len (Long/valueOf (-> newres :headers (get "Content-Length")))
+(defn save-response
+  [result-atom newres url]
+  (let [len (if-let [hlen (-> newres :headers (get "Content-Length"))]
+              (Long/valueOf hlen)
+              (count (:body newres)))
         status (:status newres)
         request-time (:request-time newres)]
 ;    (log/info "len:" len)
-
 ;    (log/info "status type:" (type status))
 ;    (log/info "request-time type:" (type request-time))
-    (conj avalue (-> {}
-                     (assoc :status status)
-                     (assoc :request-time request-time)
-                     (assoc :len len)
-                     (assoc :url url)))))
+    (swap! result-atom (fn [av r]
+                         (let [len (count (av url))]
+                           (assoc-in av [url len] r)))
+           (-> {}
+               (assoc :status status)
+               (assoc :request-time request-time)
+               (assoc :len len)))))
 
-
-(defn start-user-agent
-  [repeat-times & urls]
-  (let [cs (clj-http.cookies/cookie-store)
-        user-agent (agent [])]
-    (log/info "current thread: " (-> (Thread/currentThread) .getId))
-    (client/with-connection-pool {:timeout 5 :threads 4 :insecure? false :default-per-route 10}
-      (client/get verify-url {:cookie-store cs :as "GB2312"})
-      (client/post login-url  {:form-params {:fLoginVerification 1
-                                           :Username "weisj"
-                                           :Password "680012"
-                                           :DBPath "/domcfg.nsf"
-                                           :Path_Info "/index.nsf"
-                                           :Path_Info_Decoded "/index.nsf"
-                                           :SaveOptions 1
-                                           :$PublicAccess 1}
-                             :cookie-store cs})
-      (dotimes [_ repeat-times]
-        (doseq [url urls]
-          (send-off user-agent parse-response (client/get url {:cookie-store cs}) url))))
-    user-agent))
+(defn- fetch-urls
+  "fetch list of urls"
+  [result-atom cs urls]
+  (let [nurls (map #(cstr/replace % "{{rand}}" (random-str 8)) urls)]
+    (doall
+     (map (fn [nurl url]
+            (save-response result-atom (client/get nurl {:cookie-store cs}) url))
+            nurls urls))))
 
 (defn report-result
   [rlist]
@@ -63,19 +47,102 @@
                                       (inc m) m))
                                   0 flatlist))))
 
+(defn do-login
+  [userdefs]
+  (doseq [ud userdefs]
+    (if-let [lgf (first ud)]
+      (lgf (last ud)))))
 
-(defn start-req
-  [unums rnums]
-  (let [custom-pool (Executors/newFixedThreadPool unums)
-        uagents (map (fn [_] (agent [])) (range unums))
-        starttime (System/currentTimeMillis)]
-    (doseq [ua uagents]
-      (send-via custom-pool ua conj (start-user-agent rnums no-end-url)))
-;return list of agent list.
-;    (apply await uagents)
+(defn prepare-users
+  [result-atom userdefs]
+  (doall (map (fn [ud] (let [urls (ud 1)]
+                         (doall (map #(swap! result-atom assoc % []) urls))
+                        )) userdefs))
+  (let [nuserdefs (map (fn [ud]
+                         (conj ud (clj-http.cookies/cookie-store)))
+                       userdefs)
+        users (apply concat (map #(repeat (% 2) %) nuserdefs))]
+    (swap! result-atom assoc :thread-ids #{})
+    (do-login nuserdefs)
+    users))
+
+(defn do-requests
+  "this is a template, you can define other users too.it return a list of response list"
+  [result-atom cs urls repeat-times]
+  (client/with-connection-pool {:timeout 5 :threads 1 :insecure? false :default-per-route 10}
+    (swap! result-atom (fn [av tid]
+                         (let [tids (:thread-ids av)]
+                           (assoc av :thread-ids (conj tids tid))))
+                         (-> (Thread/currentThread) .getId))
+    (dotimes [_ repeat-times]
+      (fetch-urls result-atom cs urls))))
+
+(defn benchmark
+  "How many user repeat how many times.
+  userdefs: [login-fn urls num rpt] [login-fn urls num rpt]
+  user: [nil urls 10 10 #<BasicCookieStore []>]"
+  [& userdefs]
+  (let [result-atom (atom {})
+        users (prepare-users result-atom userdefs)
+        starttime (System/currentTimeMillis)
+        futures (map #(future-call (partial do-requests result-atom (last %) (second %) (% 3))) users)]
+    (doall futures)
+    (doall (map deref futures))
+    (log/info @result-atom)
+    (log/info "total time costs: " (- (System/currentTimeMillis) starttime))
+    result-atom))
+
+
+
+(defn oa-user-login
+  [cs]
+  (let [verify-url "http://oa.fh.gov.cn/domcfg.nsf/LoginVerification?OpenAgent&Ran=0.7505126900505275&user=weisj&pd=680012"
+        login-url "http://oa.fh.gov.cn/names.nsf?Login"]
+    (client/get verify-url {:cookie-store cs :as "GB2312"})
+    (client/post login-url {:form-params {:fLoginVerification 1
+                                          :Username "weisj"
+                                          :Password "680012"
+                                          :DBPath "/domcfg.nsf"
+                                          :Path_Info "/index.nsf"
+                                          :Path_Info_Decoded "/index.nsf"
+                                          :SaveOptions 1
+                                          :$PublicAccess 1}
+                            :cookie-store cs})))
+(def oa-user-request-urls
+  ["http://oa.fh.gov.cn/jwoa4share/jwoa4system/db_printview.nsf/PeoplePrintView?OpenAgent&infoid=mqhb_Info&path=jwoa4share/jwoa4app&dbname=db_mqhb.nsf"
+   "http://oa.fh.gov.cn/jwoa4share/jwoa4app/db_mqhb.nsf/TopBottomFrameSetWin?OpenForm&path=jwoa4share/jwoa4app/db_mqhb.nsf&fTitle=%E6%B0%91%E6%83%85%E4%BC%9A%E5%8A%9E&RndStr={{rand}}"
+   "http://oa.fh.gov.cn/jwoa4share/jwoa4system/db_publicaffair.nsf/Toppic?OpenForm&RndStr={{rand}}"])
+
+
+
+(benchmark [oa-user-login oa-user-request-urls 2 1])
+
+
+
+;(defn prepare-users
+;  [userdefs]
+;  (let [nuserdefs (map (fn [ud]
+;                         (conj ud (clj-http.cookies/cookie-store)))
+;                       userdefs)
+;        tnum (reduce #(+ %1 (%2 2)) 0 nuserdefs)
+;        custom-pool (Executors/newFixedThreadPool tnum)
+;        uagents (map #(agent %) (repeat tnum {}))
+;        users (apply concat (map #(repeat (% 2) %) nuserdefs))]
+;;    (clojure.core/set-agent-send-off-executor! (Executors/newFixedThreadPool tnum))
+;    (do-login nuserdefs)
+;    [custom-pool uagents users]))
+
+;(defn benchmark
+;  "How many user repeat how many times.
+;  userdefs: [login-fn request-fn num rpt] [login-fn request-fn num rpt]
+;  user: [nil baidu-user-request 10 10 #<BasicCookieStore []>]"
+;  [& userdefs]
+;  (let [[custom-pool uagents users] (prepare-users userdefs)
+;        starttime (System/currentTimeMillis)]
+;    (apply await (doall (map (fn [ua user]
+;                  (send-off ua
+;                     (fn [_ res] res)
+;                     ((second user) (last user) (user 3)))) uagents users)))
 ;    (report-result (map deref uagents))
-;    (log/info "this test cost time: " (- (System/currentTimeMillis) starttime) "ms")
-    (log/info uagents)
-    ))
+;    (log/info "total time costs: " (- (System/currentTimeMillis) starttime))))
 
-(start-req 20 5)
---
+++ test/compojure_start/dutil/t_pressure_util.clj
@@ -0,0 +1,53 @@
+(ns compojure-start.dutil.t-pressure-util
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [clojure.string :as cstr]
+            [compojure-start.dutil.pressure-util :as pressure-util]
+            [clojure.tools.logging :as log]
+            [clj-http.client :as client]))
+
+
+(against-background
+ [(around :facts (let [prepared (pressure-util/prepare-users (atom {}) [[nil ["http://www.baidu.com"] 10 10]])] ?form))]
+ (facts "userdefs test"
+       (fact "should return 10 users"
+             (count prepared) => 10)
+        (fact "each item is like [nil baidu-user-request 10 10 #<BasicCookieStore []>]"
+              (filter #(not= 5 (count %)) prepared) => ())
+        ))
+
+(against-background
+ [(around :facts (let [ag (agent 0)] ?form))]
+ (facts
+  (fact "when aget call value function"
+        (let [n (System/currentTimeMillis)]
+          (send ag + (future (Thread/sleep 1000) 100))
+          (< (- (System/currentTimeMillis) n) 30) => true)
+   )))
+
+(against-background
+ [(around :facts (let [x 0] ?form))]
+ (facts
+  (fact "lazy map will cause future to exec?"
+        (let [n (System/currentTimeMillis)
+              futures (map #(future (Thread/sleep 10) %) (range 5))]
+          (doseq [_ futures] nil)
+          (Thread/sleep 1000)
+          (count (filter (complement realized?) futures)) => 0
+          ))))
+
+(fact "replace string"
+      (cstr/replace "abc{{rand}}cd{{rand}}" #"\{\{rand\}\}" "1") => "abc1cd1"
+      (cstr/replace "abc{{rand}}cd{{rand}}" "{{rand}}" "1") => "abc1cd1"
+      (cstr/replace "abc{{rand}}cd{{rand}}" "{{rand1}}" "1") => "abc{{rand}}cd{{rand}}"
+      (count (pressure-util/random-str 8)) => 8
+      (count (cstr/replace "abc{{rand}}cd{{rand}}" "{{rand}}" (pressure-util/random-str 8))) => 21)
+
+(fact "test baidu home page"
+      (let [urls ["http://www.baidu.com"]
+            result-atom (pressure-util/benchmark [nil urls 10 10])]
+        (count @result-atom) => 2
+        (count (:thread-ids @result-atom)) => 10
+        (let [ress (vals (dissoc @result-atom :thread-ids)) ]
+          (doseq [r ress]
+            (count r) => 100 ))))
